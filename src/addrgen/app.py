0a7ab3a0297d6c0c9fab4d924e2d6e2e96abaa36
Steve Zheng <steve.zheng@gmail.com>
2023-08-15 14:22:17 +0000

    Auth works now.

--- src/addrgen/app.py
+++ src/addrgen/app.py
@@ -1,8 +1,11 @@
 import json
 
 def lambda_handler(event, context):
-    print("In Addrgen Handler")
+    # get context from authorizer for key "cur_user"
+    
+    cur_user = event['requestContext']['authorizer']['cur_user']
+    body_json = json.loads(event['body'])
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Addrgen Lambda!')
+        'body': f'Hello from Addrgen Lambda! User: {cur_user}, Request: {body_json}'
     }
--- src/auth/app.py
+++ src/auth/app.py
@@ -1,5 +1,32 @@
 import json
+import boto3
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    print("In Auth Handler")
-    return True
+    method_arn = event['methodArn']
+    auth_token = event['authorizationToken']
+    # method_arn = arn:aws:execute-api:us-east-1:12345668:abcd1234/ESTestInvoke-stage/GET/
+    
+    # get "us-east-1" from method_arn
+    region = method_arn.split(':')[3]
+    # get "12345668" from method_arn
+    account_id = method_arn.split(':')[4]
+    # get "abcd1234" from method_arn
+    api_id = method_arn.split(':')[5].split('/')[0]
+    
+    return {
+        "principalId": "user",
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api:Invoke",
+                    "Effect": "Allow",
+                    "Resource": f"arn:aws:execute-api:{region}:{account_id}:{api_id}/*/*"
+                }
+            ]
+        },
+        "context": {
+            "cur_user": "steve"
+        }
+    }
