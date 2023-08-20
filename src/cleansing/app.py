23a7f0e5efad8b7e66ff0649437f8d67271cd1de
Steve Zheng <steve.zheng@gmail.com>
2023-08-20 22:10:48 +0000

    - Cleanse function is done. - Add sam yaml customized to vscode setting.json to avoid warnings.

--- src/cleansing/app.py
+++ src/cleansing/app.py
@@ -1,8 +1,13 @@
 import json
 import re
 import os
+import time
 from typing import Optional
-from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+import openai
+from aws_lambda_powertools.event_handler import (
+    APIGatewayRestResolver,
+    Response
+)
 from aws_lambda_powertools.event_handler.exceptions import (
     BadRequestError,
     InternalServerError,
@@ -23,20 +28,52 @@ def openai_handler():
     if _ is None:
         raise BadRequestError("Missing required parameter")
     
-    addresses_dict = json.loads(_)
-    cleansed_addresses_dict = {}
-    # change each item in the dict
-    for key, value in addresses_dict.items():
-        # create a dict {original: str, cleansed: {apt_nbr: str, strt_nbr: str, strt_nm: str, prov: str, cntry:str, pstl_cd: str}}
-        cleansed_address = {"apt_nbr": "", "strt_nbr": "", "strt_nm": "", "prov": "", "cntry":"", "pstl": ""}
-        cleansed_addresses_dict[key] = {"original": value, "cleansed": cleansed_address}
+    # get parameters from the request body
+    body_json = json.loads(_)
+    addresses_dict = body_json["addresses"]
+    params = body_json["params"]
     
+    # prepare openai request
+    pre_process_instructuions = "translate it to English" if params["translate_output"].lower() == "true" else "keep the original language"
     openai_key = os.environ['openai_key']
     sys_mpt = os.environ['sys_mpt']
     usr_mpt = os.environ['usr_mpt']
+    usr_mpt = usr_mpt.replace('[[keep_or_translate]]', pre_process_instructuions)
+    # use ensure_ascii=False to keep non-English characters
+    usr_mpt += re.sub('\n', '', json.dumps(addresses_dict, ensure_ascii=False, separators=(',', ':')))
     
-    # keep the original language
-    # translate it to English 
+    cleansed_addresses = ""
     
-    print(re.sub('\n', '', json.dumps(cleansed_addresses_dict   , separators=(',', ':'))))
-    return cleansed_addresses_dict
+    # set openai api key and execute openai request
+    openai.api_key = openai_key
+    # try 5 times if openai is not available
+    for i in range(5):
+        try:
+            chat_completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system","content": sys_mpt},
+                    {"role": "user", "content": usr_mpt}
+                ],
+                temperature=0
+            )
+
+            cleansed_addresses = chat_completion.choices[0].message.content.strip()
+            #print(f"addresses: {cleansed_addresses}")
+            #split the addresses by newline
+            if cleansed_addresses != "":
+                break
+        except Exception as e:
+            try_again_in = 2*(i+1)
+            print(f"OpenAI is not available, try again in {try_again_in} seconds, {5-i} times left")
+            time.sleep(2*(i+1))
+            continue
+    
+    #print(cleansed_addresses)
+
+    # we cannot return JSON here because the cleansed_addresses may have non-English characters
+    return Response(
+        status_code=200,
+        content_type="application/json",
+        body = cleansed_addresses
+    )
