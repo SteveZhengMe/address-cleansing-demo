e31d30629077a9b9ab2cc66fa3d193f9ed2c3aaf
Steve Zheng <steve.zheng@gmail.com>
2023-09-07 20:09:06 +0000

    - Remove github copilot, replaced with Bito - Add a system variable SWAGGERHUB_DOMAIN to update_swaggerhub.sh and Gitpod user variables settings

--- update_swaggerhub.sh
+++ update_swaggerhub.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-swaggerhub api:update SteveZheng/AddressCleanseDemo --setdefault --file ./swagger/openapi.yaml
+swaggerhub api:update ${SWAGGERHUB_DOMAIN}/AddressCleanseDemo --setdefault --file ./swagger/openapi.yaml
 
 # get the error from swaggerhub:
 if [ $? -ne 0 ]; then
     echo "Update Failed. Create a new version of the API"
-    swaggerhub api:create SteveZheng/AddressCleanseDemo --setdefault --file ./swagger/openapi.yaml
+    swaggerhub api:create ${SWAGGERHUB_DOMAIN}/AddressCleanseDemo --setdefault --file ./swagger/openapi.yaml
     exit 0
 fi
